d0c3cb4a0409cf8d9c88cdf413f89dab2c319368
zainbhatti98 <zakwanbhatti958@gmail.com>
2026-03-12 10:28:51 +0500

    Java lab2 programs

---
+++ src/VariablesDemo.java
@@ -0,0 +1,15 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+        int age = 25;
+        double salary = 55000.50;
+        char grade = 'A';
+        boolean isActive = true;
+        String name = "Zakwan";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Active: " + isActive);
+    }
+}
